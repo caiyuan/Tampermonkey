f4b9fd33f4752c4cca30caed31ccc430f10e872d
caiyuan <caiyuan@live.com>
2021-10-13 10:57:50 +0800

    Update video-playback-rate.user.js
    major browsers still cap it at 16.

--- video-playback-rate.user.js
+++ video-playback-rate.user.js
@@ -39,12 +39,10 @@
 
     videoPlaybackRate.innerHTML = `
         <div rate="1" style="${rateButtionStyle}">原始</div>
+        <div rate="3" style="${rateButtionStyle}">3x</div>
         <div rate="5" style="${rateButtionStyle}">5x</div>
         <div rate="10" style="${rateButtionStyle}">10x</div>
-        <div rate="15" style="${rateButtionStyle}">15x</div>
-        <div rate="20" style="${rateButtionStyle}">20x</div>
-        <div rate="25" style="${rateButtionStyle}">25x</div>
-        <div rate="30" style="${rateButtionStyle}">30x</div>
+        <div rate="16" style="${rateButtionStyle}">16x</div>
         `;
 
     let rateButtionCollection = videoPlaybackRate.getElementsByTagName("div");
