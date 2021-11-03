b41f4559d01b15c29f8570f4a6ec487d4dbb6a5b
caiyuan <caiyuan@live.com>
2021-11-03 12:43:40 +0800

    Update video-playback-rate.user.js

--- video-playback-rate.user.js
+++ video-playback-rate.user.js
@@ -55,6 +55,7 @@
         <div volume="0" style="${rateButtionStyle}">音量</div>
         <div id="rate-display" rate="1" style="${rateButtionStyle}">倍速</div>
 
+        <div class="rate-fast" rate="3" style="${rateButtionStyle}">3x</div>
         <div class="rate-fast" rate="5" style="${rateButtionStyle}">5x</div>
         <div class="rate-fast" rate="10" style="${rateButtionStyle}">10x</div>
         <div class="rate-fast" rate="16" style="${rateButtionStyle}">16x</div>
