df586478734a64830c230f8492a0255e62b79c6c
caiyuan <caiyuan@live.com>
2022-03-28 13:42:11 +0800

    Update weread-typesetting.user.js

--- weread-typesetting.user.js
+++ weread-typesetting.user.js
@@ -11,6 +11,12 @@
 (function() {
     'use strict';
 
+    /* 字体 */
     GM_addStyle(".readerChapterContent * {font-family: 'FZSHENGSKS_ZHUNJW--GB1-0','MicrosoftYaHei','PingFang SC'; !important;}");
+
+    /* 背景 */
+    GM_addStyle("html body.wr_whiteTheme {background-color: #EDF4EC;}");
+    GM_addStyle(".wr_whiteTheme .readerContent .app_content {background-color: #f7f7f7;}");
+    GM_addStyle(".wr_whiteTheme .readerContent .readerTopBar {background-color: #f7f7f7;}");
 
 })();
