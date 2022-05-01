b6790d69c3e960e0df5cb0a2c0da7f75a23e16b4
caiyuan <caiyuan@live.com>
2022-05-01 16:02:34 +0800

    Update media-playback-rate.user.js

--- media-playback-rate.user.js
+++ media-playback-rate.user.js
@@ -64,6 +64,7 @@
         <div class="rate-slow" rate="1.3" style="${rateButtionStyle}">1.3x</div>
         <div class="rate-slow" rate="1.5" style="${rateButtionStyle}">1.5x</div>
         <div class="rate-slow" rate="1.7" style="${rateButtionStyle}">1.7x</div>
+        <div class="rate-slow" rate="2" style="${rateButtionStyle}">2x</div>
         `;
 
     // 控制器切换功能
