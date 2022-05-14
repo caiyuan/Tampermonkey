1b11d72ea0b6a5c674ed54b568b75eea557ef9af
caiyuan <caiyuan@live.com>
2022-05-14 11:02:47 +0800

    Update and rename douban-movie.user.js to douban-imdb.user.js

--- douban-imdb.user.js
+++ douban-imdb.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         douban-movie
+// @name         douban-imdb
 // @namespace    https://github.com/caiyuan/Tampermonkey
 // @version      0.1
 // @description  豆瓣电影评分扩展
