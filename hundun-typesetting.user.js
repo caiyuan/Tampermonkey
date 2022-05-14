436c5a048d5f45b0983b5b9035e821ba2a9cd16a
caiyuan <caiyuan@live.com>
2022-05-14 10:58:43 +0800

    Create hundun-typesetting.user.js

---
+++ hundun-typesetting.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name             hundun-typesetting
+// @namespace    https://github.com/caiyuan/Tampermonkey
+// @version          0.1
+// @description    混沌学园版面重排
+// @author           Ryan
+// @match            https://www.hundun.cn/course/*
+// @grant             GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(".navigation-container {display: none;}");
+
+})();
