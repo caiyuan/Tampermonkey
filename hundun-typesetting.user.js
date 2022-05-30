0c21f59a99d38cfb112d2da0f2943585dd80a599
caiyuan <caiyuan@live.com>
2022-05-30 19:32:44 +0800

    Update hundun-typesetting.user.js

--- hundun-typesetting.user.js
+++ hundun-typesetting.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name             hundun-typesetting
-// @namespace    https://github.com/caiyuan/Tampermonkey
-// @version          0.1
-// @description    混沌学园版面重排
-// @author           Ryan
-// @include          /^https://www.hundun.cn/course/\w+$/
+// @name              hundun-typesetting
+// @namespace         https://github.com/caiyuan/Tampermonkey
+// @version           0.1
+// @description       混沌学园版面重排
+// @author            Ryan
+// @include           /^https://www.hundun.cn/course/\w+$/
 // @grant             GM_addStyle
+// @run-at            document-end
 // ==/UserScript==
 
 (function() {
