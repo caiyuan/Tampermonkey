f9289a99129f978aeeca36f4d2ab5616f009ead8
caiyuan <caiyuan@live.com>
2022-06-04 12:36:29 +0800

    Update yunpub-typesetting.user.js

--- yunpub-typesetting.user.js
+++ yunpub-typesetting.user.js
@@ -1,21 +1,24 @@
 // ==UserScript==
-// @name         yunpub-typesetting
-// @namespace    https://github.com/caiyuan/Tampermonkey
-// @version      0.1
-// @description  中信书院排版优化
-// @author       Ryan
-// @include      https://v.yunpub.cn/*/ebook/*
-// @grant        GM_addStyle
+// @name        yunpub-typesetting
+// @namespace   https://github.com/caiyuan/Tampermonkey
+// @version     0.1
+// @description 中信书院排版优化
+// @author      Ryan
+// @include     https://v.yunpub.cn/*/ebook/*
+// @grant       GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     /* 字体 */
-    GM_addStyle("body {font-family: 'FZSHENGSKS_ZHUNJW--GB1-0','MicrosoftYaHei','PingFang SC' !important;}");
-    GM_addStyle("body p {font-size: 20px !important;}");
+    GM_addStyle("body {font-family: 'FZSHENGSKS_ZHUNJW--GB1-0','MicrosoftYaHei','PingFang SC';}");
+    GM_addStyle("body>p {font-size: 22px; color: #353535;}");
 
     /* 背景 */
-    GM_addStyle("body, .content-wrap {background-color: #f7f1e4 !important;}");
+    GM_addStyle("body {background-color: #f7f1e4 !important;}");
+    GM_addStyle("#wrapper {background-color: #35363a !important;}");
+    GM_addStyle(".content-wrap {background-color: #f7f1e4 !important;}");
+    GM_addStyle(".content-wrap {width: 1000px !important; margin: auto; padding: 0 30px;}");
 
 })();
