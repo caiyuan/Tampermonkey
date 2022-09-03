259ced9e6e0d2aa762c4f1f4495730c67bde9358
caiyuan <caiyuan@live.com>
2022-09-03 10:46:26 +0800

    Update weread-typesetting.user.js

--- weread-typesetting.user.js
+++ weread-typesetting.user.js
@@ -14,10 +14,17 @@
     /* 字体 */
     GM_addStyle(".readerChapterContent * {font-family: 'FZSHENGSKS_ZHUNJW--GB1-0','MicrosoftYaHei','PingFang SC' !important;}");
 
-    /* 背景 */
+    /* 浅色 */
     GM_addStyle("html body.wr_whiteTheme {background-color: #35363a;}");
     GM_addStyle(".wr_whiteTheme .readerContent .app_content {background-color: #f7f1e4;}");
     GM_addStyle(".wr_whiteTheme .readerContent .readerTopBar {background-color: #f7f1e4;}");
     GM_addStyle(".wr_whiteTheme .readerContent .readerFooter button {background-color: #f7f1e4;}");
+
+    /* 页宽 */
+    GM_addStyle(".readerContent .app_content {max-width: 70%;}");
+    GM_addStyle(".readerContent .readerTopBar {width: 70%; max-width: none;}");
+    GM_addStyle(".readerContent .readerControls {left: 60%;}");
+    GM_addStyle(".readerContent .readerCatalog {left: 60%;}");
+    GM_addStyle(".readerContent .readerNotePanel {left: 60%;}");
 
 })();
