60961d5385a2dc0165b446d96e8fb640077cc6aa
caiyuan <caiyuan@live.com>
2022-09-18 11:10:01 +0800

    fix

--- ffmpeg-combined.sh
+++ ffmpeg-combined.sh
@@ -9,13 +9,13 @@
 video='.mp4'
 audio='.m4a'
 
-ffmpeg='./ffmpeg'
+ffmpeg='/usr/local/bin/ffmpeg'
 
 prefix='merge.'
 
 for i in `ls *$video`; do
-	v=$i
-	a=${i%$video}$audio
-	cmd=`echo $ffmpeg -i $v -i $a -c copy $prefix$v`
-	eval $cmd
+  v=$i
+  a=${i%$video}$audio
+  cmd=`echo $ffmpeg -i "$v" -i "$a" -c copy "$prefix$v"`
+  eval $cmd
 done
