16c1102a09f1eec3d47db233f8d2afd0ac105544
caiyuan <caiyuan@live.com>
2022-10-23 14:18:07 +0800

    Update media-playbackrate.user.js

--- media-playbackrate.user.js
+++ media-playbackrate.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  倍速播放器
 // @author       Ryan
-// @include      *
+// @match        *
 // @exclude      *.qq.com/*
 // @exclude      *.hundun.cn/*
 // @grant        none
