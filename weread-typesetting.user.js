26bf88ac52bde24816c54b86cef8e4b65a71246a
caiyuan <caiyuan@live.com>
2023-02-13 15:11:35 +0800

    Update weread-typesetting.user.js

--- weread-typesetting.user.js
+++ weread-typesetting.user.js
@@ -18,12 +18,4 @@
     GM_addStyle(".readerContent .readerCatalog {left: 60%;}");
     GM_addStyle(".readerContent .readerNotePanel {left: 60%;}");
 
-    /* 浅色 */
-    GM_addStyle("html body.wr_whiteTheme {background-color: #323232;}");
-    GM_addStyle(".wr_whiteTheme .readerContent * {color: #262523;}");
-    GM_addStyle(".wr_whiteTheme .readerContent .toolbarItem_text {color: #F7F1E4;}");
-    GM_addStyle(".wr_whiteTheme .readerContent .app_content {background-color: #F7F1E4;}");
-    GM_addStyle(".wr_whiteTheme .readerContent .readerTopBar {background-color: #F7F1E4;}");
-    GM_addStyle(".wr_whiteTheme .readerContent .readerFooter button {background-color: #F7F1E4; color: #B98A2B;}");
-
 })();
