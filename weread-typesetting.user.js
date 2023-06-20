b4672c59a9967fbcf8ee8051b16135796aba85ed
caiyuan <caiyuan@live.com>
2023-06-20 12:39:14 +0800

    Update weread-typesetting.user.js

--- weread-typesetting.user.js
+++ weread-typesetting.user.js
@@ -29,15 +29,15 @@
     /***** 浅色主题 *****/
 
     /* 阅读 */
-    GM_addStyle("html body.wr_whiteTheme {background-color: #b3d1b7;}");
+    GM_addStyle("html body.wr_whiteTheme {background-color: #aacbaf;}");
     GM_addStyle("html body.wr_whiteTheme .readerTopBar {background-color: #EEEEEE;}");
-    GM_addStyle("html body.wr_whiteTheme .readerContent .app_content {background-color: #EEEEEE30;}");
+    GM_addStyle("html body.wr_whiteTheme .readerContent .app_content {background-color: #EEEEEE50;}");
     GM_addStyle("html body.wr_whiteTheme .readerHeaderButton {color: #14801d;}");
     GM_addStyle("html body.wr_whiteTheme .readerFooter_button {color: #14801d;}");
     GM_addStyle("html body.wr_whiteTheme .readerChapterContent {color: #222222;}");
     /* 书架 */
-    GM_addStyle("html body.wr_whiteTheme .app_fullHeight {background-color: #b3d1b7;}");
-    GM_addStyle("html body.wr_whiteTheme .navBar {background-color: #b3d1b7;}");
-    GM_addStyle("html body.wr_whiteTheme .navBar_border {border: solid #2128321a; border-width: 0 0 1px;}");
+    GM_addStyle("html body.wr_whiteTheme .app_fullHeight {background-color: #aacbaf;}");
+    GM_addStyle("html body.wr_whiteTheme .navBar {background-color: #aacbaf;}");
+    GM_addStyle("html body.wr_whiteTheme .navBar_border {border: none;}");
 
 })();
