5d8e7fa162ae1f678d947cd921af29640765d7f4
caiyuan <caiyuan@live.com>
2023-07-03 10:07:53 +0800

    Update calibre-viewer.user.js

--- calibre-viewer.user.js
+++ calibre-viewer.user.js
@@ -17,8 +17,8 @@
     GM_addStyle("div[id$='overlay'] {z-index: 901;}");
 
     GM_addStyle("#book-read-aloud-overlay {z-index: 899;}");
-    GM_addStyle("#book-read-aloud-overlay>div {margin: 2rem !important;}");
-    GM_addStyle("#book-read-aloud-overlay>div {opacity: 0.15 !important;}");
-    GM_addStyle("#book-read-aloud-overlay>div:hover {opacity: 1 !important;}");
+    GM_addStyle("#book-read-aloud-overlay div[id*='bar'] {margin: 2rem !important;}");
+    GM_addStyle("#book-read-aloud-overlay div[id*='bar'] {opacity: 0.15 !important;}");
+    GM_addStyle("#book-read-aloud-overlay div[id*='bar']:hover {opacity: 1 !important;}");
 
 })();
