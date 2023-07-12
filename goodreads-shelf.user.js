29719207b500be51d09dfa856b2afe3d208fa1cd
caiyuan <caiyuan@live.com>
2023-07-12 09:46:45 +0800

    Update goodreads-shelf.user.js

--- goodreads-shelf.user.js
+++ goodreads-shelf.user.js
@@ -14,21 +14,23 @@
     document.querySelectorAll(".elementList").forEach(function(e){
         try {
 
-            // 滤掉2013年前书籍
+            // 书籍粗筛
 
-            var s = e.querySelector(".smallText").textContent.trim();
-            var y = s.substring(s.length-4);
-            if(y<2013){e.setAttribute("style","display:none")};
+            var ss = e.querySelector(".smallText").textContent.trim().replace(/(\n)|(,)|(   )/g,"").replace(/  /g," ").split(" ");
+            var avgRating = ss[2];
+            var ratings = ss[4];
+            var published = ss[8];
+            if(avgRating<3.5 || ratings<3000 || published<2013){ e.setAttribute("style","display:none"); };
 
 
-            // 添加豆瓣快捷搜索
+            // 豆瓣搜索
 
             var i = e.querySelector(".leftAlignedImage");
             var t = i.getAttribute("title");
             i.setAttribute("target","_blank");
             i.setAttribute("href","https://search.douban.com/book/subject_search?search_text="+t);
 
-        } catch(error){}
+        } catch(error) {}
     });
 
 })();
