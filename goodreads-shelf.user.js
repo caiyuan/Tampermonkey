0c52f6fff6b74d15481029c59d05db0ad20ab789
caiyuan <caiyuan@live.com>
2023-08-22 11:18:38 +0800

    Update goodreads-shelf.user.js

--- goodreads-shelf.user.js
+++ goodreads-shelf.user.js
@@ -11,26 +11,23 @@
 (function() {
     'use strict';
 
-    document.querySelectorAll(".elementList").forEach(function(e){
+    document.querySelectorAll(".elementList").forEach(function(e) {
         try {
-
             // 书籍粗筛
-
-            var ss = e.querySelector(".smallText").textContent.trim().replace(/(\n)|(,)|(   )/g,"").replace(/  /g," ").split(" ");
+            var ss = e.querySelector(".smallText").textContent.trim().replace(/(\n)|(,)|(   )/g, "").replace(/  /g, " ").split(" ");
             var avgRating = ss[2];
             var ratings = ss[4];
             var published = ss[8];
-            if(avgRating<3.7 || ratings<3000 || published<2013){ e.setAttribute("style","display:none"); };
-
+            if (avgRating < 3.7 || ratings < 3000) {
+                e.setAttribute("style", "display:none");
+            }
 
             // 豆瓣搜索
-
             var i = e.querySelector(".leftAlignedImage");
             var t = i.getAttribute("title");
-            i.setAttribute("target","_blank");
-            i.setAttribute("href","https://search.douban.com/book/subject_search?search_text="+t);
-
-        } catch(error) {}
+            i.setAttribute("target", "_blank");
+            i.setAttribute("href", "https://search.douban.com/book/subject_search?search_text=" + t);
+        } catch (error) {}
     });
 
 })();
