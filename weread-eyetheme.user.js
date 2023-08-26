38e762af80d3fe0f681874e9ee10605b2ae3cde3
caiyuan <caiyuan@live.com>
2023-08-26 21:46:00 +0800

    Update weread-eyetheme.user.js

--- weread-eyetheme.user.js
+++ weread-eyetheme.user.js
@@ -35,28 +35,28 @@
         // 通用配置
         const commonStyles = `
             /* 页宽 */
-            .routerView .readerTopBar {max-width: 1200px; height: 48px;}
-            .routerView .app_content {max-width: 1200px;}
+            #routerView .readerTopBar {max-width: 1200px; height: 48px;}
+            #routerView .app_content {max-width: 1200px;}
             /* 控制 */
-            .routerView .readerControls {margin-left: 648px;}
-            .routerView .readerCatalog {margin-left: 80px;}
-            .routerView .readerNotePanel {margin-left: 80px;}
+            #routerView .readerControls {margin-left: 648px;}
+            #routerView .readerCatalog {margin-left: 80px;}
+            #routerView .readerNotePanel {margin-left: 80px;}
             /* 字体 */
-            .routerView * {font-family: 'HarmonyOS Sans SC';}
-            .routerView .readerTopBar * {font-family: sans-serif; font-weight: 500;}
-            .routerView .readerTopBar .readerTopBar_title_chapter {font-style: italic; font-weight: 400;}
+            #routerView * {font-family: 'HarmonyOS Sans SC';}
+            #routerView .readerTopBar * {font-family: sans-serif; font-weight: 500;}
+            #routerView .readerTopBar .readerTopBar_title_chapter {font-style: italic; font-weight: 400;}
             /* 颜色 */
-            .routerView .app_content .readerChapterContent {color: #b2b2b2;}
+            #routerView .app_content .readerChapterContent {color: #b2b2b2;}
         `;
 
         // 护眼绿色
         const greenStyles = `
             html body.wr_whiteTheme {background-color: #94b399;}
-            html body.wr_whiteTheme .routerView .app_content {background-color: #a6c9ab;}
-            html body.wr_whiteTheme .routerView .app_content .readerTopBar {background-color: #ffffff;}
-            html body.wr_whiteTheme .routerView .app_content .readerChapterContent {color: #333333;}
-            html body.wr_whiteTheme .routerView .app_content .readerHeaderButton {color: #14801d;}
-            html body.wr_whiteTheme .routerView .app_content .readerFooter_button {color: #14801d;}
+            html body.wr_whiteTheme #routerView .app_content {background-color: #a6c9ab;}
+            html body.wr_whiteTheme #routerView .app_content .readerTopBar {background-color: #ffffff;}
+            html body.wr_whiteTheme #routerView .app_content .readerChapterContent {color: #333333;}
+            html body.wr_whiteTheme #routerView .app_content .readerHeaderButton {color: #14801d;}
+            html body.wr_whiteTheme #routerView .app_content .readerFooter_button {color: #14801d;}
         `;
 
         GM_addStyle(commonStyles);
@@ -68,7 +68,7 @@
 
         // 通用配置
         const commonStyles = `
-            .routerView .app_content {max-width: 1200px !important;}
+            .app_content {max-width: 1200px !important;}
         `;
 
         GM_addStyle(commonStyles);
