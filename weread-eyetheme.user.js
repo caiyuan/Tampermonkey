4c42366fcdda97a1b4466523d692631071354879
caiyuan <caiyuan@live.com>
2023-10-30 13:48:28 +0800

    Update weread-eyetheme.user.js

--- weread-eyetheme.user.js
+++ weread-eyetheme.user.js
@@ -47,9 +47,9 @@
             #routerView .app_content .readerTopBar .readerTopBar_title_chapter {font-style: italic; font-weight: 400;}
             /* 内容文字 */
             #routerView .app_content .readerChapterContent {color: #b2b2b2;}
-            #routerView .app_content .readerChapterContent [class] {font-family: '汉仪书仿';}
+            #routerView .app_content .readerChapterContent [class] {font-family: '汉仪书仿 55W';}
             #routerView .app_content .readerChapterContent [class*='Title'] {font-family: '汉仪新蒂佛塔书';}
-            #routerView .app_content .readerChapterContent [class*='content'] {font-family: '汉仪书仿';}
+            #routerView .app_content .readerChapterContent [class*='content'] {font-family: '汉仪书仿 55W';}
             #routerView .app_content .readerChapterContent [class*='quotation'] {font-family: '汉仪楷体 S';}
             /* 翻页文字 */
             #routerView .app_content .readerHeaderButton {font-family: '汉仪老字号';}
