857dc106cc14e7c210306c27d0b14032ffc436f4
caiyuan <caiyuan@live.com>
2023-11-05 14:15:11 +0800

    Update goodreads-shelf.user.js

--- goodreads-shelf.user.js
+++ goodreads-shelf.user.js
@@ -11,22 +11,29 @@
 (function() {
     'use strict';
 
-    document.querySelectorAll(".elementList").forEach(function(e) {
+    document.querySelectorAll(".elementList").forEach(function(el) {
         try {
+
             // 书籍粗筛
-            var ss = e.querySelector(".smallText").textContent.trim().replace(/(\n)|(,)|(   )/g, "").replace(/  /g, " ").split(" ");
-            var avgRating = ss[2];
-            var ratings = ss[4];
-            var published = ss[8];
-            if (avgRating < 3.7 || ratings < 3000 || published < 2015) {
-                e.setAttribute("style", "display:none");
+
+            var info = el.querySelector(".smallText").textContent.trim().replace(/(\n)|(,)|(   )/g, "").replace(/  /g, " ").split(" ");
+            var avgRating = info[2];
+            var ratings = info[4];
+            var published = info[8];
+            if (avgRating < 3.7 || ratings < 3000 || published < 2010) {
+                el.setAttribute("style", "display:none");
             }
 
+
             // 豆瓣搜索
-            var i = e.querySelector(".leftAlignedImage");
-            var t = i.getAttribute("title");
-            i.setAttribute("target", "_blank");
-            i.setAttribute("href", "https://search.douban.com/book/subject_search?search_text=" + t);
+
+            var search = "https://search.douban.com/book/subject_search?search_text=";
+
+            var cover = el.querySelector(".leftAlignedImage");
+            var title = cover.getAttribute("title");
+            cover.setAttribute("target", "_blank");
+            cover.setAttribute("href", search + title);
+
         } catch (error) {}
     });
 
