44b91121ba2961315c2950587aa4fa162c015d3c
caiyuan <caiyuan@live.com>
2023-11-06 12:59:14 +0800

    Update goodreads-shelf.user.js

--- goodreads-shelf.user.js
+++ goodreads-shelf.user.js
@@ -26,19 +26,22 @@
 
             // 图书检索
             var douban = "https://search.douban.com/book/subject_search?search_text={q}";
-            var books = "https://search.books.com.tw/search/query/key/{q}/cat/EBA";
+            var books = "https://search.books.com.tw/search/query/key/{q}/cat/BKA";
+            var kobo = "https://www.kobo.com/tw/zh/search?query={q}";
 
             var cover = el.querySelector(".leftAlignedImage");
             var title = cover.getAttribute("title");
             cover.setAttribute("href", "#");
             cover.setAttribute("data-douban", douban.replace("{q}", title));
             cover.setAttribute("data-books", books.replace("{q}", title));
+            cover.setAttribute("data-kobo", kobo.replace("{q}", title));
 
             cover.addEventListener("click", function(event) {
-                const { douban, books } = this.dataset;
+                const { douban, books, kobo } = this.dataset;
 
                 window.open(douban, '_blank');
                 window.open(books, '_blank');
+                window.open(kobo, '_blank');
             });
 
         } catch (error) {}
