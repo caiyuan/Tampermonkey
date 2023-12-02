017915ed774754b8d87b228a0265fd65d5b8195c
caiyuan <caiyuan@live.com>
2023-12-02 16:43:12 +0800

    Create kindle-dl.user.js

---
+++ kindle/kindle-dl.user.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name         kindle-dl
+// @namespace    https://github.com/caiyuan/Tampermonkey
+// @version      0.1
+// @description  kindle 电子书下载
+// @author       Ryan
+// @match        https://www.amazon.cn/hz/mycd/digital-console/contentlist/booksAll/dateDsc/*
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // 构建下载面板
+
+    const ebookControlPanel = document.createElement("div");
+    ebookControlPanel.id = "ebookControlPanel";
+    ebookControlPanel.style.cssText = `
+    z-index: 10001 !important;
+    position: fixed !important;
+    top: 10%;
+    right: 30px;
+  `;
+
+    ebookControlPanel.innerHTML = `
+    <div>
+      <button id="ebook:dl" style="display: inline-block; cursor: pointer; padding: 11px 13px 13px; border-radius: 5px; margin: 0px; vertical-align: middle; text-align: center; margin-inline-start: 10px; border: 1px solid; color: white; background: linear-gradient(rgb(90, 0, 0), rgb(0, 0, 0)); min-height: 12px; line-height: 12px; min-width: 10px;">自動下載</button>
+    </div>
+  `;
+
+    document.body.appendChild(ebookControlPanel);
+
+    // 操作下载功能
+
+    var dl = document.getElementById("ebook:dl");
+    dl.addEventListener("click", function () {
+        console.log('downloading ...');
+        dl.textContent = '下載中·';
+        dl.style.color = 'gray';
+        dl.disabled = true;
+
+        dlAction();
+    });
+
+    // 执行下载过程
+
+    var pages = 77;
+    var page = 1;
+    var books = [];
+
+    function processBook(page, index) {
+        if (index == 0) {
+            books = document.querySelectorAll("#CONTENT_LIST input[id$=':KindleEBook']");
+        }
+
+        var eid = books[index].id;
+        var bid = eid.substring(0, eid.indexOf(':'));
+        var title = document.querySelector('#content-title-' + bid).textContent;
+
+        console.log(page + "::" + index + "::" + bid + "::" + title);
+
+        document.querySelector('#DOWNLOAD_AND_TRANSFER_ACTION_' + bid).click();
+        document.querySelector('#download_and_transfer_list_' + bid + '_0').click();
+        document.querySelector('#DOWNLOAD_AND_TRANSFER_ACTION_' + bid + '_CONFIRM').click();
+
+        if (index < books.length - 1) {
+            setTimeout(function () {
+                document.querySelector('#notification-close').click();
+                processBook(page, index + 1);
+            }, 5000);
+        } else if (++page <= pages) {
+            setTimeout(function () {
+                document.querySelector('#notification-close').click();
+                document.querySelector('#page-' + page).click();
+                setTimeout(function () {
+                    processBook(page, 0);
+                }, 10000);
+            }, 5000);
+        }
+    }
+
+    function dlAction() {
+        setTimeout(function () {
+            document.querySelector('#page-' + page).click();
+            setTimeout(function () {
+                processBook(page, 0);
+            }, 10000);
+        }, 0);
+    }
+
+})();
