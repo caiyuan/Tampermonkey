ba58ca2fb613b2b7c1b7de76bda9567f10df34d9
caiyuan <caiyuan@live.com>
2025-04-13 09:55:24 +0800

    Create weread-winktemplate.user.js

---
+++ weread-winktemplate.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         weread-winktemplate
+// @namespace    https://github.com/caiyuan/Tampermonkey
+// @version      0.1
+// @description  微信阅读 AI 问书
+// @author       Ryan
+// @match        https://weread.qq.com/*
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    GM_addStyle("#multi-turn-stream-container * {font-family: 'LXGW WenKai GB'; font-size: 1.05rem; -webkit-font-smoothing: subpixel-antialiased;}");
+
+    GM_addStyle(".readerAIChatPanel {width: 860px !important; margin-left: -360px !important;}");
+
+})();
