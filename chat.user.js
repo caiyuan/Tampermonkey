1de237b6ad266ebf32d7d9cf95562a59e3db1f1a
caiyuan <caiyuan@live.com>
2025-11-04 13:01:27 +0800

    Create chat.user.js

---
+++ chat.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         chat
+// @namespace    https://github.com/caiyuan/Tampermonkey
+// @version      0.1
+// @description  fonts
+// @author       Ryan
+// @match        https://yuanbao.tencent.com/*
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle("* {font-family: 'LXGW WenKai Screen' !important;}");
+
+})();
