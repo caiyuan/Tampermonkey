d523520cad2814cb0dbb88cf68c0ec7a34974bd9
caiyuan <caiyuan@live.com>
2025-11-21 12:42:58 +0800

    Update wechat-news.user.js

--- wechat-news.user.js
+++ wechat-news.user.js
@@ -14,7 +14,7 @@
 
     GM_addStyle("* {font-family: 'LXGW WenKai' !important;}");
 
-    GM_addStyle("html {filter: invert(1) hue-rotate(180deg);}");
+    GM_addStyle("html {filter: invert(1) hue-rotate(180deg) saturate(1.3) contrast(1.3); transform: translateZ(0);}");
     GM_addStyle("video, img {filter: invert(1) hue-rotate(180deg);}");
 
 })();
