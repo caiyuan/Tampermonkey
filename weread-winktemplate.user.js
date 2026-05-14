3be4e932aeb8004861689906f3bbfd95e8ee7959
caiyuan <caiyuan@live.com>
2026-05-14 12:25:16 +0800

    Adjust filter and add font size for readerTopBar

--- weread-winktemplate.user.js
+++ weread-winktemplate.user.js
@@ -13,7 +13,9 @@
 (function() {
     'use strict';
 
-    GM_addStyle(".readerTopBar {font-family: 'LXGW WenKai GB Screen'; height: 35px; filter: invert(10%);}");
+    GM_addStyle(".readerTopBar {font-family: 'LXGW WenKai GB Screen'; height: 35px; filter: invert(7%);}");
+    GM_addStyle(".readerTopBar * {font-size: 0.75rem;}");
+
     GM_addStyle("#multi-turn-stream-container * {font-family: 'LXGW WenKai GB Screen';}");
 
     GM_addStyle("@media (max-width: 1023px) {.readerContent .app_content, .app_content>div {max-width: 670px;}}");
